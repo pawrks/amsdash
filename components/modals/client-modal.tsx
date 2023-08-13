5b5fe799eb29e296121f775cd7e4218b0d9a04e3
parker <prwilliams5@gmail.com>
2023-08-13 11:37:43 -0500

    Added form functionality with zod validation.

--- components/modals/client-modal.tsx
+++ components/modals/client-modal.tsx
@@ -1,23 +1,80 @@
 'use client'
 
 import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { useClientModal } from '@/hooks/use-client-modal'
 import { Modal } from '@/components/ui/modal'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 // In order to use this modal throughout the  app it will be 'provided'
 // via a 'provider' in the provider directory.
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: 'Name requires at least 1 character.'
+  })
+})
+
 export const ClientModal = () => {
-  const ClientModal = useClientModal()
+  const clientModal = useClientModal()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: ''
+    }
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    // TODO: Create Client
+  }
 
   return (
     <Modal
-      title="Create a new Client"
+      title="Create New Client"
       description="Add a new client to get started!"
-      isOpen={ClientModal.isOpen}
-      onClose={ClientModal.onClose}
+      isOpen={clientModal.isOpen}
+      onClose={clientModal.onClose}
     >
-      (pending) Create Client Form (pending)
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Client name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={clientModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   )
 }
