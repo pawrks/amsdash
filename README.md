bf54576f531fb4c7709ac868227a229930edbf0d
parker <prwilliams5@gmail.com>
2023-08-16 13:43:29 -0500

    added PATCH & DELETE routes for clients in settings page

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# Getting Started
+# CMSdash
 
-Run development server.
+This project is in the process of building a content/product management system.
+There are plans to integrate many different tools and it is a work in progress. There
+will be more information available after the app has more functionality built in.
 
-```bash
-npm run dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your web browser.
+Stack inclues Next.js with app router, shadcn/ui, and an SQL database (MySQL with PlanetScale).
--- app/(dashboard)/[clientId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[clientId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { useForm } from 'react-hook-form'
 import * as z from 'zod'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Client } from '@prisma/client'
 import { Trash } from 'lucide-react'
@@ -9,31 +11,86 @@ import { Trash } from 'lucide-react'
 import { Heading } from '@/components/ui/heading'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
+import { Input } from '@/components/ui/input'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
 
 interface SettingsFormProps {
   initialData: Client
 }
 
 const formSchema = z.object({
-  name: z.string().min(1)
+  name: z.string().min(1, {
+    message: 'Name requires at least 1 character.'
+  })
 })
 
-type SettingsFormValues = z.infer<typeof FormSchema>
+type SettingsFormValues = z.infer<typeof formSchema>
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const form = useForm<SettingsFormValues>({
-    resolver: zodResolver
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
   })
 
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    try {
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
+  }
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage client preferences" />
-        <Button variant="destructive" size="sm" onClick={() => {}}>
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="sm"
+          onClick={() => setOpen(true)}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Client name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   )
 }
