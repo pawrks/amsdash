3f47c2a784fd9c8c94b51d3fa5873c157d59e581
parker <prwilliams5@gmail.com>
2023-08-30 09:08:59 -0500

    added access list and contact models to db schema

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,23 @@
-import { UserButton, auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { UserButton, auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-import StoreSwitcher from "@/components/store-switcher";
-import { MainNav } from "@/components/main-nav-test";
-import { ThemeToggle } from "@/components/theme-toggle";
-import prismadb from "@/lib/prismadb";
+import StoreSwitcher from '@/components/store-switcher'
+import { MainNav } from '@/components/main-nav'
+import { ThemeToggle } from '@/components/theme-toggle'
+import prismadb from '@/lib/prismadb'
 
 const Navbar = async () => {
-  const { userId } = auth();
+  const { userId } = auth()
 
   if (!userId) {
-    redirect("/sign-in");
+    redirect('/sign-in')
   }
 
   const stores = await prismadb.store.findMany({
     where: {
-      userId,
-    },
-  });
+      userId
+    }
+  })
 
   return (
     <div className="border-b">
@@ -30,7 +30,7 @@ const Navbar = async () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
