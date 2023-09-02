8a8c16a8cfeceaf791c7ed153bc29079f2eaa340
parker <prwilliams5@gmail.com>
2023-09-02 08:15:35 -0500

    fixed lengths and data rates product form issues

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,51 +1,65 @@
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb'
 
-import { ProductForm } from "./components/product-form";
+import { ProductForm } from './components/product-form'
 
 const ProductPage = async ({
   params
 }: {
-  params: { productId: string, storeId: string }
+  params: { productId: string; storeId: string }
 }) => {
   const product = await prismadb.product.findUnique({
     where: {
-      id: params.productId,
+      id: params.productId
     },
     include: {
-      images: true,
+      images: true
     }
-  });
+  })
 
   const categories = await prismadb.category.findMany({
     where: {
-      storeId: params.storeId,
-    },
-  });
+      storeId: params.storeId
+    }
+  })
 
   const sizes = await prismadb.size.findMany({
     where: {
-      storeId: params.storeId,
-    },
-  });
+      storeId: params.storeId
+    }
+  })
 
   const colors = await prismadb.color.findMany({
     where: {
-      storeId: params.storeId,
-    },
-  });
+      storeId: params.storeId
+    }
+  })
 
-  return ( 
+  const lengths = await prismadb.length.findMany({
+    where: {
+      storeId: params.storeId
+    }
+  })
+
+  const dataRates = await prismadb.dataRate.findMany({
+    where: {
+      storeId: params.storeId
+    }
+  })
+
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm 
-          categories={categories} 
+        <ProductForm
+          categories={categories}
           colors={colors}
           sizes={sizes}
+          lengths={lengths}
+          dataRates={dataRates}
           initialData={product}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default ProductPage;
+export default ProductPage
